2ff287553c756e644874845e4f596db2be4a2048
sema karan <karan18sema@gmail.com>
2024-04-27 17:11:38 +0200

    solve reverse string problem

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/ruby/reverse_string.rb
@@ -1,5 +1,8 @@
 def reverse_string(str)
   # type your code in here
+  reversed_array = []
+  str.chars.each { |char| reversed_array.unshift(char) }
+  return reversed_array.join('')
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -12,7 +15,13 @@ if __FILE__ == $PROGRAM_NAME
   puts "=>", reverse_string('catbaby')
 
   # Don't forget to add your own!
+  puts "Expecting: 'ames m'I"
+  puts "=>", reverse_string("I'm sema")
+
 end
 
 # Please add your pseudocode to this file
+# Iterate through each character
+# At every character to the index 0
+
 # And a written explanation of your solution
