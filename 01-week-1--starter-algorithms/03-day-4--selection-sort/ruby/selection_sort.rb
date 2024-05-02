9c883cc17d480f5074b7c6dcc39ed52168f79653
sema karan <karan18sema@gmail.com>
2024-05-02 10:26:21 +0200

    solve selection sort

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/ruby/selection_sort.rb
@@ -1,5 +1,12 @@
 def selection_sort(arr)
   # type your code in here
+  sorted = []
+  until arr.empty?
+    arr_min = arr.min
+    sorted.push(arr_min)
+    arr.delete_at(arr.index(arr_min))
+  end
+  sorted
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -12,10 +19,16 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 
   # BENCHMARK HERE, you can print the average runtime
-  long_input = []
+  # long_input = []
 
-  100.times { long_input << rand }
+  # 100.times { long_input << rand }
 end
 
 # Please add your pseudocode to this file
+# declare an empty array
+# iterate array until array is empty
+# get the min and add it to the sorted array
+# delete it from the given array
+# return sorted array
+
 # And a written explanation of your solution
